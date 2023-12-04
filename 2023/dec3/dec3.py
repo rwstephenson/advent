9472966a74afb4b844f68cee19e4965970e62229
Rob Stephenson <rwstephe@spotify.com>
2023-12-04 07:33:31 +0100

    day 3 and 4

---
+++ 2023/dec3/dec3.py
@@ -0,0 +1,124 @@
+possibleGears = {}
+
+def calculateGearRatios(possibleGears):
+    sum = 0
+    for posGear in possibleGears:
+        if len(possibleGears[posGear]) == 2:
+            sum += possibleGears[posGear][0]*possibleGears[posGear][1]
+    return sum
+
+def checkGears(row,i,num):
+    gearKey = str(row)+","+str(i)
+    if (gearKey not in possibleGears):
+        possibleGears[gearKey] = [num]
+    else:
+        possibleGears[gearKey].append(num)
+
+def isSymbol(row,i,c, num):
+    if c in digitSet:
+        return False
+    elif c == '*':
+        checkGears(row,i,num)
+        return True
+    elif c == '.':
+        return False
+    elif c == '\n':
+        return False
+    else:
+        return True
+
+def checkHorizontal(schema, row, numStart, numEnd, num):
+    isPart = False
+    if numStart > 0 and isSymbol(row,numStart-1,schema[row][numStart - 1], num):
+        print ("Found symbol: " + schema[row][numStart - 1])
+        isPart = True
+    if numEnd + 1 < len(schema[row]) and isSymbol(row,numEnd+1,schema[row][numEnd + 1], num):
+        print ("Found symbol: " + schema[row][numEnd + 1])
+        isePart = True
+    return isPart
+
+def checkVertical(schema, row, numStart, numEnd, num):
+    isPart = False
+    if row > 0:
+        for i in range(numStart, numEnd+1):
+            if isSymbol(row-1,i,schema[row-1][i], num):
+                print ("Found symbol above: " + schema[row-1][i])
+                isPart = True
+    if row + 1 < len(schema):
+        for i in range(numStart, numEnd+1):
+            if isSymbol(row+1,i,schema[row+1][i], num):
+                print ("Found symbol below: " + schema[row+1][i])
+                isPart = True
+    return isPart
+
+def checkDiagonal(schema, row, numStart, numEnd, num):
+    isPart = False
+    if row > 0 and numStart > 0 and isSymbol(row-1,numStart-1,schema[row-1][numStart-1], num):
+        print ("Found symbol diagonally! case 1")
+        isPart = True
+    if row > 0 and numEnd + 1 < len(schema[row]) and isSymbol(row-1,numEnd+1,schema[row-1][numEnd+1], num):
+        print ("Found symbol diagonally! case 2")
+        isPart = True
+    if row + 1 < len(schema) and numStart > 0 and isSymbol(row+1,numStart-1,schema[row+1][numStart-1], num):
+        print ("Found symbol diagonally! case 3")
+        isPart = True
+    if row + 1 < len(schema) and numEnd + 1 < len(schema[row]) and isSymbol(row+1,numEnd+1,schema[row+1][numEnd+1], num):
+        print ("Found symbol diagonally! case 4")
+        isPart = True
+    return isPart
+
+def checkForNearbySymbol(schema, row, numStart, numEnd, num):
+    return checkHorizontal(schema, row, numStart, numEnd, num) or checkVertical(schema, row, numStart, numEnd, num) or checkDiagonal(schema, row, numStart, numEnd, num)
+
+digitSet = {'0','1','2','3','4','5','6','7','8','9'}
+
+def parseFilePt1(filename):
+    schema = []
+    with open(filename, "r") as f:
+        for line in f:
+            schema.append(line)
+    sum = 0
+    totalSum = 0
+    sumIgnored = 0
+    for l in range(len(schema)):
+        print ("Line: " + str(l) + "-------------------------------------")
+        lineLength = len(schema[l])
+        numIdx = 0
+        numEndIdx = 0
+        numberString = ""
+        c = 0
+        while c < lineLength:
+            if len(numberString) == 0:
+                # not currently finding a number
+                if schema[l][c] in digitSet:
+                    # number found
+                    numIdx = c
+                    numberString = schema[l][c]
+            else:
+                if schema[l][c] in digitSet:
+                    # still finding number
+                    numberString = numberString + schema[l][c]
+                else:
+                    # end of number
+                    numEndIdx = c - 1
+                    print ("Found number: " + numberString)
+                    totalSum += int(numberString)
+                    if (checkForNearbySymbol(schema, l, numIdx, numEndIdx,int(numberString))):
+                        sum += int(numberString)
+                        print("And the sum is: " + str(sum))
+                    else:
+                        print ("Number with NO symbol:" + numberString)
+                        sumIgnored += int(numberString)
+                    numberString = ""
+                    numEndIdx = 0
+                    numIdx = 0
+            c = c + 1
+    print("---------------")
+    print ("Total Sum: " + str(totalSum))
+    print ("Sum Parts: " + str(sum))
+    print ("Sum Ignored: " + str(sumIgnored))
+    print ("Total - Ignored: " + str(totalSum - sumIgnored))
+
+    print(calculateGearRatios(possibleGears))
+
+parseFilePt1("input.txt")
---
+++ 2023/dec4/dec4.py
@@ -0,0 +1,74 @@
+
+def getBonus(line):
+    cardid = line.split(":")[0]
+    input = line.split(":")[1].strip()
+    winners = set(input.split("|")[0].strip().split(" "))
+    cardStrings = input.split("|")[1].strip().split(" ")
+
+    cardEntries = []
+    numFound = 0
+    for entry in cardStrings:
+        if entry != '':
+            e = entry
+            if entry in winners:
+                numFound += 1
+    print ("Found " + str(numFound) + " matches for " + cardid)
+    return numFound
+
+def getRoundPoints(line):
+    input = line.split(":")[1].strip()
+    winners = set(input.split("|")[0].strip().split(" "))
+    cardStrings = input.split("|")[1].strip().split(" ")
+
+    cardEntries = []
+    numFound = 0
+    for entry in cardStrings:
+        if entry != '':
+            e = entry
+            if entry in winners:
+                numFound += 1
+    if numFound > 0:
+        return 2**(numFound - 1)
+    else:
+        return 0
+
+def parseFile(filename):
+    with open(filename, "r") as f:
+        sum = 0
+        for line in f:
+            sum += getRoundPoints(line)
+    return sum
+
+bonusCards = {}
+
+def incrementBonusCards(id, matches, numCards):
+    newCards = 0
+    if matches > 0:
+        for i in range(matches):
+            if id+i+1 not in bonusCards:
+                bonusCards[id+i+1] = numCards
+                print ("Adding " + str(numCards) + " new " + str(id+i+1) + " cards")
+            else:
+                bonusCards[id+i+1] = bonusCards[id+i+1] + numCards
+            newCards += numCards
+    print ("Had " + str(numCards) + " copies of card " + str(id) + ", increasing the next " + str(matches))
+    return newCards
+
+def parseFilePt2(filename):
+    with open(filename, "r") as f:
+        totalCards = 0
+        row = 1
+        id = 1
+        for line in f:
+            matches = getBonus(line)
+            numCards = 1
+            if id in bonusCards:
+                numCards = bonusCards[id] + 1
+            totalCards += numCards #adding original
+            incrementBonusCards(id, matches, numCards)
+            id += 1
+    return totalCards
+
+
+print(parseFilePt2("input.txt"))
+
