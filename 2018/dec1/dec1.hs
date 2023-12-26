4c968ead4b078b1b0b374fbdf6a5e6b8e7d7ebaf
Rob Stephenson <rwstephe@spotify.com>
2023-12-26 22:39:04 +0100

    2018 dec1 haskell

---
+++ 2018/dec1/dec1.hs
@@ -0,0 +1,28 @@
+import Data.Set (Set,insert,member,empty)
+
+parseInt :: String -> Int
+parseInt ('+':num) = read num
+parseInt num       = read num
+
+sumFile :: [String] -> Int
+sumFile entries = sum (map parseInt entries)
+
+findRepeat :: Int -> Set Int -> [Int] -> Int
+findRepeat total seen (num:rest) | member total seen = total
+                                 | otherwise         = findRepeat (total+num) (insert total seen) rest
+findRepeat _ _ _                                     = -1337
+((
+pt1 :: IO ()
+pt1 = do
+  contents <- readFile "input.txt"
+  let entries = lines contents
+  let result = sumFile entries
+  print result
+
+pt2 :: IO ()
+pt2 = do
+  contents <- readFile "input.txt"
+  let entries = lines contents
+  let result = findRepeat 0 empty (cycle (map parseInt entries))
+  print result
+
