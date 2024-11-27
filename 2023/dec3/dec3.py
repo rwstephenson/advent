504c4c8e5ebbf529a055b1929439b7c3688f3334
Rob Stephenson <rwstephe@spotify.com>
2024-11-27 07:54:32 +0100

    update 2023 day 3 with point class

--- 2023/dec3/dec3.py
+++ 2023/dec3/dec3.py
@@ -1,127 +1,55 @@
-possibleGears = {}
+import sys
+from math import *
+from collections import *
+sys.path.append('../../lib')
+from point import Point
 
-def calculateGearRatios(possibleGears):
-    sum = 0
-    for posGear in possibleGears:
-        if len(possibleGears[posGear]) == 2:
-            sum += possibleGears[posGear][0]*possibleGears[posGear][1]
-    return sum
-
-def checkGears(row,i,num):
-    gearKey = str(row)+","+str(i)
-    if (gearKey not in possibleGears):
-        possibleGears[gearKey] = [num]
-    else:
-        possibleGears[gearKey].append(num)
-
-def isSymbol(row,i,c, num):
-    if c in digitSet:
-        return False
-    elif c == '*':
-        checkGears(row,i,num)
-        return True
-    elif c == '.':
-        return False
-    elif c == '\n':
-        return False
-    else:
-        return True
-
-def checkHorizontal(schema, row, numStart, numEnd, num):
-    isPart = False
-    if numStart > 0 and isSymbol(row,numStart-1,schema[row][numStart - 1], num):
-        print ("Found symbol: " + schema[row][numStart - 1])
-        isPart = True
-    if numEnd + 1 < len(schema[row]) and isSymbol(row,numEnd+1,schema[row][numEnd + 1], num):
-        print ("Found symbol: " + schema[row][numEnd + 1])
-        isePart = True
-    return isPart
-
-def checkVertical(schema, row, numStart, numEnd, num):
-    isPart = False
-    if row > 0:
-        for i in range(numStart, numEnd+1):
-            if isSymbol(row-1,i,schema[row-1][i], num):
-                print ("Found symbol above: " + schema[row-1][i])
-                isPart = True
-    if row + 1 < len(schema):
-        for i in range(numStart, numEnd+1):
-            if isSymbol(row+1,i,schema[row+1][i], num):
-                print ("Found symbol below: " + schema[row+1][i])
-                isPart = True
-    return isPart
-
-def checkDiagonal(schema, row, numStart, numEnd, num):
-    isPart = False
-    if row > 0 and numStart > 0 and isSymbol(row-1,numStart-1,schema[row-1][numStart-1], num):
-        print ("Found symbol diagonally! case 1")
-        isPart = True
-    if row > 0 and numEnd + 1 < len(schema[row]) and isSymbol(row-1,numEnd+1,schema[row-1][numEnd+1], num):
-        print ("Found symbol diagonally! case 2")
-        isPart = True
-    if row + 1 < len(schema) and numStart > 0 and isSymbol(row+1,numStart-1,schema[row+1][numStart-1], num):
-        print ("Found symbol diagonally! case 3")
-        isPart = True
-    if row + 1 < len(schema) and numEnd + 1 < len(schema[row]) and isSymbol(row+1,numEnd+1,schema[row+1][numEnd+1], num):
-        print ("Found symbol diagonally! case 4")
-        isPart = True
-    return isPart
-
-def checkForNearbySymbol(schema, row, numStart, numEnd, num):
-    return checkHorizontal(schema, row, numStart, numEnd, num) or checkVertical(schema, row, numStart, numEnd, num) or checkDiagonal(schema, row, numStart, numEnd, num)
-
-digitSet = {'0','1','2','3','4','5','6','7','8','9'}
-
-def parseFilePt1(filename):
-    schema = []
-    with open(filename, "r") as f:
+def parseGrid(filename):
+    grid = []
+    with open(filename,"r") as f:
         for line in f:
-            schema.append(line)
-    sum = 0
-    totalSum = 0
-    sumIgnored = 0
-    count = 0
-    for l in range(len(schema)):
-        print ("Line: " + str(l) + "-------------------------------------")
-        lineLength = len(schema[l])
-        numIdx = 0
-        numEndIdx = 0
-        numberString = ""
-        c = 0
-        while c < lineLength:
-            if len(numberString) == 0:
-                # not currently finding a number
-                if schema[l][c] in digitSet:
-                    # number found
-                    numIdx = c
-                    numberString = schema[l][c]
+            ## Adding padding to the end to break parts on newlines
+            grid.append(line.strip() + '.')
+    return grid
+
+def hasNeighboorSymbol(p):
+    for n in p.getNeighboors(True):
+        if n.value != '.' and not n.value.isdigit():
+            return n
+    return None
+
+def findGearValues(possibleGears):
+    total = 0
+    for gear in possibleGears:
+        if len(possibleGears[gear]) == 2:
+            total += prod(possibleGears[gear])
+    return total
+
+def solve(filename):
+    grid = parseGrid(filename)
+    parts = []
+    magnitude = 0
+    partValue = 0
+    possibleGears = defaultdict(list)
+    partSymbol = None
+    for y in range(len(grid)):
+        for x in reversed(range(len(grid[0]))):
+            p = Point(x,y,grid)
+            if p.value.isdigit():
+                if partSymbol is None:
+                    partSymbol = hasNeighboorSymbol(p)
+                partValue += int(p.value)*10**magnitude
+                magnitude += 1
             else:
-                if schema[l][c] in digitSet:
-                    # still finding number
-                    numberString = numberString + schema[l][c]
-                else:
-                    # end of number
-                    numEndIdx = c - 1
-                    print ("Found number: " + numberString)
-                    totalSum += int(numberString)
-                    if (checkForNearbySymbol(schema, l, numIdx, numEndIdx,int(numberString))):
-                        sum += int(numberString)
-                        count += 1
-                        print("And the sum is: " + str(sum))
-                    else:
-                        print ("Number with NO symbol:" + numberString)
-                        sumIgnored += int(numberString)
-                    numberString = ""
-                    numEndIdx = 0
-                    numIdx = 0
-            c = c + 1
-    print("---------------")
-    print("Count: " + str(count))
-    print ("Total Sum: " + str(totalSum))
-    print ("Sum Parts: " + str(sum))
-    print ("Sum Ignored: " + str(sumIgnored))
-    print ("Total - Ignored: " + str(totalSum - sumIgnored))
+                if partValue != 0:
+                    if partSymbol:
+                        parts.append(partValue)
+                        if partSymbol.value == '*':
+                            possibleGears[hash(partSymbol)].append(partValue)
+                    partSymbol = None
+                partValue = 0
+                magnitude = 0
+    return sum(parts),findGearValues(possibleGears)
 
-    print(calculateGearRatios(possibleGears))
-
-parseFilePt1("input.txt")
+res = solve("input.txt")
+print("Pt1: {}, Pt2: {}".format(res[0],res[1]))
--- lib/point.py
+++ lib/point.py
@@ -3,6 +3,9 @@ class Point:
     def __init__(self, x, y, grid):
         self.x = x
         self.y = y
+        self.value = grid[y][x]
+        self.maxX = len(grid[0]) - 1
+        self.maxY = len(grid) - 1
         self.grid = grid
 
     def __str__(self):
@@ -17,16 +20,26 @@ class Point:
     def getNeighboor(self, d):
         if d == 'N' and self.y > 0:
             return Point(self.x, self.y - 1, self.grid)
-        elif d == 'S' and self.y < len(self.grid) - 1:
+        elif d == 'S' and self.y < self.maxY:
             return Point(self.x, self.y + 1, self.grid)
-        elif d == 'E' and self.x > 0:
+        elif d == 'W' and self.x > 0:
             return Point(self.x-1, self.y, self.grid)
-        elif d == 'W' and self.x < len(self.grid[0])-1:
+        elif d == 'E' and self.x < self.maxX:
             return Point(self.x+1, self.y, self.grid)
+        elif d == 'NW' and self.y > 0 and self.x > 0:
+            return Point(self.x-1, self.y-1, self.grid)
+        elif d == 'NE' and self.y > 0 and self.x < self.maxX:
+            return Point(self.x+1, self.y-1, self.grid)
+        elif d == 'SW' and self.y < self.maxY and self.x > 0:
+            return Point(self.x-1, self.y+1, self.grid)
+        elif d == 'SE' and self.y < self.maxY and self.x < self.maxX:
+            return Point(self.x+1, self.y+1, self.grid)
         return None
 
     def getNeighboors(self, diagonally=False):
         dirs = ['N','S','E','W']
+        if diagonally:
+            dirs += ['NE','SE','NW','SW']
         neighboors = [i for i in list(map(self.getNeighboor,dirs)) if i is not None]
         return neighboors
 
