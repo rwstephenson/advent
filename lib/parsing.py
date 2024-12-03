e3957a20b9deed4f50d5bfb9cf8f4f693c6ba9c5
Rob Stephenson <rwstephe@spotify.com>
2024-12-03 12:27:56 +0100

    cleanup

--- lib/parsing.py
+++ lib/parsing.py
@@ -5,6 +5,9 @@ _integer_pattern = re.compile("-?[0-9]+")
 def getInts(line):
     return [int(m) for m in _integer_pattern.findall(line)]
 
+def findAll(string,substring):
+    return [m.start() for m in re.finditer(substring, string)]
+
 def parseLines(filename):
     lines = []
     with open(filename,"r") as f:
