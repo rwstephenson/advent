49f5e8d8b6b7d6a94535eca2fe4031ef579f0174
Rob Stephenson <rwstephe@spotify.com>
2024-12-03 07:36:22 +0100

    day 3 messy

--- lib/parsing.py
+++ lib/parsing.py
@@ -1,5 +1,14 @@
 import re
 
 _integer_pattern = re.compile("-?[0-9]+")
+
 def getInts(line):
     return [int(m) for m in _integer_pattern.findall(line)]
+
+def parseLines(filename):
+    lines = []
+    with open(filename,"r") as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
