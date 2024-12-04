4452606db0ee12ec4c4c9f3fb4fcfc5ad6306228
Rob Stephenson <rwstephe@spotify.com>
2024-12-04 07:38:40 +0100

    less naive

--- 2024/dec4/dec4.py
+++ 2024/dec4/dec4.py
@@ -8,73 +8,55 @@ from parsing import *
 
 def findXMAS(x):
     dirs = ['N','S','E','W','NE','NW','SE','SW']
+    MAS = 'MAS'
     xmas = 0
     for d in dirs:
-        m = x.getNeighboor(d)
-        if m and m.value == 'M':
-            a = m.getNeighboor(d)
-            if a and a.value == 'A':
-                s = a.getNeighboor(d)
-                if s and s.value == 'S':
+        p = x
+        for i in range(len(MAS)):
+            p = p.getNeighboor(d)
+            if p and p.value == MAS[i]:
+                if i == len(MAS) - 1:
                     xmas += 1
+            else:
+                break
     return xmas
 
 def findMASX(a):
-    m1dirs = ['NE','NW','SE','SW']
+    dirs = ['NE','NW','SE','SW']
+    neighboors = []
     masx = 0
-    for m1dir in m1dirs:
-        m2dir = s1dir = s2dir = ''
-        if m1dir == 'NE':
-            m2dir = 'SE'
-            s1dir = 'NW'
-            s2dir = 'SW'
-        elif m1dir == 'SE':
-            m2dir = 'SW'
-            s1dir = 'NW'
-            s2dir = 'NE'
-        elif m1dir == 'SW':
-            m2dir = 'NW'
-            s1dir = 'NE'
-            s2dir = 'SE'
-        elif m1dir == 'NW':
-            m2dir = 'NE'
-            s1dir = 'SE'
-            s2dir = 'SW'
-        else:
-            assert(False)
-        m1 = a.getNeighboor(m1dir)
-        m2 = a.getNeighboor(m2dir)
-        s1 = a.getNeighboor(s1dir)
-        s2 = a.getNeighboor(s2dir)
-        if (m1 and m2 and s1 and s2) and (m1.value == 'M' and m2.value == 'M' and s1.value == 'S' and s2.value == 'S'):
-            masx += 1
+    for d in dirs:
+        n = a.getNeighboor(d)
+        if n:
+            neighboors.append(n.value)
+    sCount = neighboors.count('S')
+    mCount = neighboors.count('M')
+    if sCount == 2 and mCount == 2 and neighboors[0] != neighboors[3]:
+        masx += 1
     return masx
 
-def solve(filename):
+def solve(filename,pt):
     grid = []
     total = 0
     with open(filename,"r") as f:
         for line in f:
             grid.append(line.strip())
         for row in range(len(grid)):
-            xs = findAll(grid[row],'X')
-            for x in xs:
-                total += findXMAS(Point(x,row,grid))
+            if pt == 1:
+                xs = findAll(grid[row],'X')
+                for x in xs:
+                    total += findXMAS(Point(x,row,grid))
+            elif pt == 2:
+                xs = findAll(grid[row],'A')
+                for a in xs:
+                    total += findMASX(Point(a,row,grid))
     return total
 
-def solve2(filename):
-    grid = []
-    total = 0
-    with open(filename,"r") as f:
-        for line in f:
-            grid.append(line.strip())
-        for row in range(len(grid)):
-            xs = findAll(grid[row],'A')
-            for a in xs:
-                total += findMASX(Point(a,row,grid))
-    return total
-
-resTest = solve2("testInput.txt")
+resTest = solve("testInput.txt",1)
+assert resTest == 18, f"Result was {resTest}"
+res = solve("input.txt",1)
+print("Part 1: Test Input: {} Puzzle Input: {}".format(resTest,res))
+resTest = solve("testInput.txt",2)
 assert resTest == 9, f"Result was {resTest}"
-res = solve2("input.txt")
-print("Test Input: {} Puzzle Input: {}".format(resTest,res))
+res = solve("input.txt",2)
+print("Part 2: Test Input: {} Puzzle Input: {}".format(resTest,res))
