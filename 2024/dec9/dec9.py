de5c0ca83898045a46e682d2032cfc3543a248f3
Rob Stephenson <rwstephe@spotify.com>
2024-12-09 11:18:36 +0100

    fix bug that made it slow

--- 2024/dec9/dec9.py
+++ 2024/dec9/dec9.py
@@ -27,8 +27,6 @@ def solve(filename, pt):
                     freeSpace.append((pos,int(c)))
             pos += int(c)
             i += 1
-    print(filePos)
-    print(freeSpace)
     maxPos = pos
     k = maxPos
     freeSpace.reverse()
@@ -36,54 +34,41 @@ def solve(filename, pt):
         if k in filePos:
             # Found a file!
             fileId,fileLength = filePos[k]
-            #print("Files: {}".format(filePos))
-            freeSpace = list(filter(lambda e: e[1] > 0,freeSpace))
             i = len(freeSpace) - 1
             while i >= 0 and fileLength > 0:
                 freeSpacePos,spaceAvailable = freeSpace[i]
-                #print("Free Space: {}".format(freeSpace))
-                print("Pos {} Found File {} length {}".format(k,fileId,fileLength))
-                print("Check open space {} length {}".format(freeSpacePos,spaceAvailable))
-                if spaceAvailable == 0 or k < freeSpacePos:
-                    i -= 1
-                    continue
-                elif freeSpacePos > k+fileLength:
-                    print("But we're at the end")
-                    filePos[k] = (fileId,fileLength)
-                    fileLength = 0
+                if k < freeSpacePos:
+                    break
                 elif fileLength > spaceAvailable:
-                    print("Not enough space for the full file")
                     if pt == 1:
                         filePos[freeSpacePos] = fileId,spaceAvailable
                         filePos[k] = (fileId,fileLength - spaceAvailable)
-                        freeSpace[i] = (freeSpacePos,0)
+                        freeSpace.pop(i)
+                        i -= 1
                         fileLength -= spaceAvailable
                     elif pt == 2:
                         i -= 1
                 else:
-                    print("Enough space for full file, Popping file {} at pos {}".format(fileId,k))
                     filePos.pop(k)
                     filePos[freeSpacePos] = fileId,fileLength
                     if spaceAvailable > fileLength:
                         freeSpace[i] = (freeSpacePos + fileLength,spaceAvailable - fileLength)
                     elif spaceAvailable == fileLength:
-                        freeSpace[i] = (freeSpacePos,0)
+                        freeSpace.pop(i)
+                        i -= 1
                     else:
                         assert(False)
                     fileLength -= spaceAvailable
         k -= 1
     total = 0
     k = 0
-    print(filePos)
     while k <= maxPos:
         if k not in filePos:
             k += 1
             continue
         curFile,curLen = filePos[k]
-        print("Pos: {} file: {} lenghth: {}".format(k,curFile,curLen))
         for i in range(curLen):
             total += k*curFile
-            print("Pos: {} * file: {} = {} Total = {}".format(k,curFile,k*curFile,total))
             k += 1
     return total
 
@@ -98,5 +83,5 @@ def run(pt,day,year,expect):
         c = "b"
     submit(res, part=c, day=day, year=year)
 
-#run(1,9,2024,1928)
+run(1,9,2024,1928)
 run(2,9,2024,2858)
