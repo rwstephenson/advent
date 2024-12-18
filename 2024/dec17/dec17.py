4ff10c426b648eadfaa704debfee63a4bafe77ce
Rob Stephenson <rwstephe@spotify.com>
2024-12-18 09:21:46 +0100

    2024 dec17

--- 2024/dec17/dec17.py
+++ 2024/dec17/dec17.py
@@ -17,25 +17,18 @@ def getCombo(operand,regs):
 
 def adv(operand,regs):
     assert(operand == 3)
-    print("Reduce A by 8x")
     regs[0] = regs[0] // (2 ** getCombo(operand,regs))
 
 def bxl(operand,regs):
-    if operand == 2:
-        print("XOR B with 10")
-    elif operand == 3:
-        print("XOR B with 11")
-    else:
+    if operand != 2 and operand != 3:
         assert(False)
     regs[1] = regs[1] ^ operand
 
 def bst(operand,regs):
-    print("B becomes A % 8")
     assert(operand == 4)
     regs[1] = getCombo(operand,regs) % 8
 
 def jnz(operand, regs):
-    print("Repeat if A isn't 0")
     if regs[0] == 0:
         return -1
     else:
@@ -43,11 +36,9 @@ def jnz(operand, regs):
         return operand
 
 def bxc(operand, regs):
-    print("XOR B and C")
     regs[1] = regs[1] ^ regs[2]
 
 def out(operand, regs):
-    print("output B % 8")
     assert(operand == 5)
     return getCombo(operand,regs) % 8
 
@@ -56,18 +47,47 @@ def bdv(operand, regs):
     regs[1] = regs[0] // (2**getCombo(operand,regs))
 
 def cdv(operand, regs):
-    print("Set C to A // (2^B)")
     assert(operand == 5)
     regs[2] = regs[0] // (2**getCombo(operand,regs))
 
+def iteration(a):
+    b = (a % 8) ^ 2
+    c = a // (2**b)
+    b = (b ^ c) ^ 3
+    return b%8
 
-#Input: 2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0
-# while a > 0
-#   B = (A % 8) ^ 2
-#   C = A % (2^B)
-#   B = (B ^ C) ^ 3
-#   print(B%8)
-#   a = a // 8
+def runProgram(a):
+    s = ""
+    while a > 0:
+        s += str(iteration(a)) + ','
+        a = a // 8
+    return s[:-1]
+
+def programToStr(program):
+    s = ""
+    for i in program:
+        s += str(i) + ','
+    return s[0:-1]
+
+def solve2(filename):
+    program = getInts(parseLines(filename)[4])
+    res = solve3(0,program.copy())
+    assert(programToStr(program) == runProgram(res))
+    return res
+
+def solve3(a,program):
+    if len(program) > 0:
+        target = program.pop()
+        for i in range(8):
+            if iteration(a+i) == target:
+                if len(program) > 0:
+                    res = solve3((a+i)*8,program.copy())
+                    if res != -1:
+                        return res
+                else:
+                    return a+i
+        return -1
+
 
 def solve(filename, pt):
     lines = parseLines(filename)
@@ -92,7 +112,8 @@ def solve(filename, pt):
         elif instr == 4:
             bxc(operand,regs)
         elif instr == 5:
-            res += str(out(operand,regs) )
+            o = out(operand,regs)
+            res += str(o)
             res += ','
         elif instr == 6:
             assert(False)
@@ -100,21 +121,22 @@ def solve(filename, pt):
         elif instr == 7:
             cdv(operand,regs)
         pc += 2
-        print(regs)
     return res[0:-1]
 
-def run(pt,day,year,expect):
+def run(pt,day,year):
     with open("input.txt","w") as f:
         f.write(get_data(day=day,year=year))
-    #resTest = solve("testInput.txt",pt)
-    #assert resTest == expect, f"Result was {resTest}"
-    res = solve("input.txt",pt)
-    print("Test Input: {} Puzzle Input: {}".format(resTest,res))
+    if pt == 1:
+        res = solve("input.txt",pt)
+        res2 = runProgram(27575648)
+        assert res2 == res, f"Result was {res2} but expected {res}"
+    elif pt == 2:
+        res = solve2("input.txt")
     if pt == 1:
         c = "a"
     else:
         c = "b"
     submit(res, part=c, day=day, year=year)
 
-run(1,17,2024,"4,6,3,5,6,3,5,2,1,0")
-#run(2,17,2024,"")
+run(1,17,2024)
+run(2,17,2024)
