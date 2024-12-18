64367cf3fad5ad7fe2ae16bbacbd93e50b02eb59
Robert Stephenson <Rob Stephenson>
2024-12-18 12:58:02 +0100

    clean up day17

--- 2024/dec17/dec17.py
+++ 2024/dec17/dec17.py
@@ -16,78 +16,31 @@ def getCombo(operand,regs):
         assert(False)
 
 def adv(operand,regs):
-    assert(operand == 3)
     regs[0] = regs[0] // (2 ** getCombo(operand,regs))
 
 def bxl(operand,regs):
-    if operand != 2 and operand != 3:
-        assert(False)
     regs[1] = regs[1] ^ operand
 
 def bst(operand,regs):
-    assert(operand == 4)
     regs[1] = getCombo(operand,regs) % 8
 
 def jnz(operand, regs):
     if regs[0] == 0:
         return -1
     else:
-        assert(operand % 2 == 0)
         return operand
 
 def bxc(operand, regs):
     regs[1] = regs[1] ^ regs[2]
 
 def out(operand, regs):
-    assert(operand == 5)
     return getCombo(operand,regs) % 8
 
 def bdv(operand, regs):
-    assert(False)
     regs[1] = regs[0] // (2**getCombo(operand,regs))
 
 def cdv(operand, regs):
-    assert(operand == 5)
     regs[2] = regs[0] // (2**getCombo(operand,regs))
-
-def iteration(a):
-    b = (a % 8) ^ 2
-    c = a // (2**b)
-    b = (b ^ c) ^ 3
-    return b%8
-
-def runProgram(a):
-    s = ""
-    while a > 0:
-        s += str(iteration(a)) + ','
-        a = a // 8
-    return s[:-1]
-
-def programToStr(program):
-    s = ""
-    for i in program:
-        s += str(i) + ','
-    return s[0:-1]
-
-def solve2(filename):
-    program = getInts(parseLines(filename)[4])
-    res = solve3(0,program.copy())
-    assert(programToStr(program) == runProgram(res))
-    return res
-
-def solve3(a,program):
-    if len(program) > 0:
-        target = program.pop()
-        for i in range(8):
-            if iteration(a+i) == target:
-                if len(program) > 0:
-                    res = solve3((a+i)*8,program.copy())
-                    if res != -1:
-                        return res
-                else:
-                    return a+i
-        return -1
-
 
 def solve(filename, pt):
     lines = parseLines(filename)
@@ -122,6 +75,44 @@ def solve(filename, pt):
             cdv(operand,regs)
         pc += 2
     return res[0:-1]
+
+def iteration(a):
+    b = (a % 8) ^ 2
+    c = a // (2**b)
+    b = (b ^ c) ^ 3
+    return b%8
+
+def runProgram(a):
+    s = ""
+    while a > 0:
+        s += str(iteration(a)) + ','
+        a = a // 8
+    return s[:-1]
+
+def programToStr(program):
+    s = ""
+    for i in program:
+        s += str(i) + ','
+    return s[0:-1]
+
+def solve2(filename):
+    program = getInts(parseLines(filename)[4])
+    res = getMinA(0,program.copy())
+    assert(programToStr(program) == runProgram(res))
+    return res
+
+def getMinA(a,program):
+    assert(len(program) > 0)
+    target = program.pop()
+    for i in range(8):
+        if iteration(a+i) == target:
+            if len(program) > 0:
+                res = getMinA((a+i)*8,program.copy())
+                if res != -1:
+                    return res
+            else:
+                return a+i
+    return -1
 
 def run(pt,day,year):
     with open("input.txt","w") as f:
