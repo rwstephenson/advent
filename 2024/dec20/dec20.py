d7e0ffc47dd174d8d60ab734746ccfef37b4a04b
Robert Stephenson <Rob Stephenson>
2024-12-20 20:23:11 +0100

    2024 dec20

--- 2024/dec20/dec20.py
+++ 2024/dec20/dec20.py
@@ -8,17 +8,14 @@ from parsing import *
 from aocd import get_data, submit
 from functools import lru_cache
 
-def getPathNeighboors(path):
-    neighboors = set()
-    for p in path:
-        pns = p.getNeighboors()
-        for n in pns:
-            neighboors.add(n)
-    return neighboors
-
-@lru_cache()
-def pathLen(s,e):
-    return len(s.bfs(e,['#']))
+def pathLen(s, e, cache):
+    if hash((s,e)) in cache:
+        return cache[hash((s,e))]
+    path = s.bfs(e,['#'])
+    dist = len(path)
+    for i in range(len(path)):
+        cache[hash((path[i],e))] = dist - i
+    return dist
 
 def getCheatEndpoints(s,seconds):
     assert(s.value != '#')
@@ -30,11 +27,10 @@ def getCheatEndpoints(s,seconds):
                 endpoints.append(p)
     return endpoints
 
-def solve2(filename, pt, savings,seconds):
+def solve2(filename, savings, seconds):
     lines = parseLines(filename)
+    cache = {}
     grid = []
-    s = None
-    e = None
     total = 0
     for r in range(len(lines)):
         sx = lines[r].find('S')
@@ -50,67 +46,32 @@ def solve2(filename, pt, savings,seconds):
     s = Point(sxy[0],sxy[1],grid)
     e = Point(exy[0],exy[1],grid)
     baselinePath = s.bfs(e,['#'])
-    baselineDist = len(baselinePath) - 1
-    print(baselineDist)
-    cheats = set()
+    baselineDist = pathLen(s,e,cache)
+    assert(len(baselinePath) == baselineDist)
     for p in baselinePath:
         endpoints = getCheatEndpoints(p,seconds)
         for endpoint in endpoints:
-            distanceToCheat = pathLen(s,p) - 1
+            distanceToCheat = pathLen(s,p,cache) - 1
             cheatDistance = p.distance(endpoint)
-            distanceFromCheat = pathLen(endpoint,e) - 1
+            distanceFromCheat = pathLen(endpoint,e,cache) - 1
             newDist = distanceToCheat + cheatDistance + distanceFromCheat
-            if baselineDist - newDist >= savings and hash((p,endpoint)) not in cheats:
-                print("Cheat from {} to {} Distance To {} Cheat Dist {} Remainig {} ".format(p, endpoint,distanceToCheat,cheatDistance,distanceFromCheat))
+            if baselineDist - newDist >= savings and hash((p,endpoint)):
+                print("Cheat from {} to {} of length {} has shortest path {} for a savings of {}".format(p,endpoint, cheatDistance, newDist, baselineDist - newDist))
                 total += 1
     return total
 
-
-def solve(filename, pt, savings):
-    lines = parseLines(filename)
-    grid = []
-    s = None
-    e = None
-    total = 0
-    for r in range(len(lines)):
-        sx = lines[r].find('S')
-        ex = lines[r].find('E')
-        if sx >= 0:
-            sxy = (sx,r)
-        if ex >= 0:
-            exy = (ex,r)
-        row = []
-        for c in range(len(lines[r])):
-            row.append(lines[r][c])
-        grid.append(row)
-    s = Point(sxy[0],sxy[1],grid)
-    e = Point(exy[0],exy[1],grid)
-    baselinePath = s.bfs(e,['#'])
-    baselineDist = len(baselinePath)
-    assert(baselineDist != 0)
-    seen = set()
-    toCheck = getPathNeighboors(baselinePath)
-    for r in range(len(lines)):
-        for c in range(len(lines[0])):
-            p = Point(c,r,grid)
-            if p.value == '#' and p in toCheck:
-                neighboors = p.getNeighboors()
-                grid[p.y][p.x] = '.'
-                newPath = set(s.bfs(e,['#']))
-                newDist = len(newPath)
-                if baselineDist - newDist >= savings and p in newPath:
-                    print("Removing {} saves {}".format(p,baselineDist - newDist))
-                    total += 1
-                seen.add(hash(p))
-                grid[p.y][p.x] = '#'
-    return total
-
-def run(pt,day,year,savings,expect):
+def run(pt,day,year,expect):
     with open("input.txt","w") as f:
         f.write(get_data(day=day,year=year))
-    resTest = solve2("testInput.txt",pt,savings,20)
+    if pt == 1:
+        seconds = 2
+        testSavings = 2
+    elif pt == 2:
+        seconds = 20
+        testSavings = 70
+    resTest = solve2("testInput.txt",testSavings,seconds)
     assert resTest == expect, f"Result was {resTest}"
-    res = solve2("input.txt",pt,100,20)
+    res = solve2("input.txt",100,seconds)
     print("Test Input: {} Puzzle Input: {}".format(resTest,res))
     if pt == 1:
         c = "a"
@@ -118,5 +79,5 @@ def run(pt,day,year,savings,expect):
         c = "b"
     submit(res, part=c, day=day, year=year)
 
-#run(1,20,2024,2,44)
-run(2,20,2024,70,41)
+run(1,20,2024,44)
+run(2,20,2024,41)
