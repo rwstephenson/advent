a95f9645dcae3e1c5c90f2087072af529f00effd
Rob Stephenson <rwstephe@spotify.com>
2024-12-25 09:52:57 +0100

    2024 dec21 messy

--- 2024/dec21/dec21.py
+++ 2024/dec21/dec21.py
@@ -23,72 +23,84 @@ def makeDirectionalGrid():
 
 def findDirpadPaths(s,e):
     if s == e:
-        return ['A']
+        return 'A'
     if s == 'A':
         if e == 'W':
-            return ['WSWA','SWWA']
+            return 'SWWA'
         elif e == 'E':
-            return ['SA']
+            return 'SA'
         elif e == 'N':
-            return ['WA']
+            return 'WA'
         elif e == 'S':
-            return ['WSA','SWA']
+            return 'WSA'
     elif s == 'N':
         if e == 'W':
-            return ['SWA']
+            return 'SWA'
         elif e == 'E':
-            return ['SEA','ESA']
+            return 'SEA'
         elif e == 'S':
-            return ['SA']
+            return 'SA'
         elif e == 'A':
-            return ['WA']
+            return 'EA'
     elif s == 'E':
         if e == 'W':
-            return ['WWA']
+            return 'WWA'
         elif e == 'A':
-            return ['NA']
+            return 'NA'
         elif e == 'N':
-            return ['NWA','WNA']
+            return 'NWA'
         elif e == 'S':
-            return ['WA']
+            return 'WA'
     elif s == 'S':
         if e == 'W':
-            return ['WA']
+            return 'WA'
         elif e == 'E':
-            return ['EA']
+            return 'EA'
         elif e == 'N':
-            return ['NA']
+            return 'NA'
         elif e == 'A':
-            return ['NEA','ENA']
+            return 'NEA'
     elif s == 'W':
         if e == 'S':
-            return ['EA']
+            return 'EA'
         elif e == 'E':
-            return ['EEA']
+            return 'EEA'
         elif e == 'N':
-            return ['ENA']
+            return 'ENA'
         elif e == 'A':
-            return ['EENA','ENEA']
+            return 'EENA'
 
 def findPath(rob,value):
+    s = rob.value
     e = rob.find(value)
     path = rob.bfs(e,['X'])
     res = ""
+    dirs = defaultdict(int)
     for p in path:
         if p != rob:
             for d in ['E','N','S','W']:
                 n = rob.getNeighboor(d)
                 if n and n == p:
                     rob = p
-                    res += d
-        if p.value == value:
-            res += 'A'
+                    dirs[d] += 1
+    dirsOrder = ['W','N','S','E']
+    if s == '0' or s == 'A':
+        if value == '7' or value == '4' or value == '1':
+            dirsOrder = ['N','W','E','S']
+    if s == '7' or s == '4' or s == '1':
+        if value == '0' or value == 'A':
+            dirsOrder = ['N','W','E','S']
+    for d in dirsOrder:
+        for i in range(dirs[d]):
+            res += d
+    res += 'A'
     return res,e
 
 def walk(s,path,grid):
     p = Point(s.x,s.y,grid)
     res = ""
     for d in path:
+        #print("{} go {}".format(p.value,d))
         if d == 'A':
             res += p.value
         elif d == '<':
@@ -101,8 +113,78 @@ def walk(s,path,grid):
             p = p.getNeighboor('S')
         else:
             p = p.getNeighboor(d)
-    print("Walk path {} presses {} len {}".format(path,res,len(res)))
+    #print("Walk path {} presses {} len {}".format(path,res,len(res)))
     return res
+
+def solve2(filename, pt):
+    print("-------------------------------------")
+    lines = parseLines(filename)
+    keypadS,keypad = makeKeypadGrid()
+    dirPadS,dirGrid = makeDirectionalGrid()
+    total = 0
+    for line in lines:
+        lineSplit = line.split(': ')
+        keypadNums = lineSplit[0]
+        keypadWalk = ""
+        dirpad1Walks = [""]
+        rob3 = Point(keypadS.x,keypadS.y,keypadS.grid)
+        rob2 = Point(dirPadS.x,dirPadS.y,dirPadS.grid)
+        rob1 = Point(dirPadS.x,dirPadS.y,dirPadS.grid)
+        for c in keypadNums:
+            rob3Path,rob3 = findPath(rob3,c)
+            keypadWalk += rob3Path
+        print(keypadWalk)
+        for c in keypadWalk:
+            rob2Paths = findDirpadPaths(rob2.value,c)
+            if len(rob2Paths) == 1:
+                for w in range(len(dirpad1Walks)):
+                    #print("From {} to {} adding {} to {}".format(rob2.value, c, rob2Paths[0],dirpad1Walks[w]))
+                    dirpad1Walks[w] += rob2Paths[0]
+            elif len(rob2Paths) == 2:
+                newDirpadWalks = []
+                for w in range(len(dirpad1Walks)):
+                    newDirpadWalks.append(dirpad1Walks[w] + rob2Paths[0])
+                    newDirpadWalks.append(dirpad1Walks[w] + rob2Paths[1])
+                dirpad1Walks = newDirpadWalks
+            rob2 = rob2.find(c)
+        newWalks = []
+        for w in dirpad1Walks:
+            dirpad2Walks = [""]
+            derivedKeypadWalk = walk(dirPadS,w,dirGrid)
+            assert(derivedKeypadWalk == keypadWalk)
+            #print("keypadWalk: {} len: {}".format(derivedKeypadWalk,len(derivedKeypadWalk)))
+            for c in w:
+                rob1Paths = findDirpadPaths(rob1.value,c)
+                if len(rob1Paths) == 1:
+                    for w in range(len(dirpad2Walks)):
+                        dirpad2Walks[w] += rob1Paths[0]
+                elif len(rob1Paths) == 2:
+                    newDirpad2Walks = []
+                    for w in range(len(dirpad2Walks)):
+                        newDirpad2Walks.append(dirpad2Walks[w] + rob1Paths[0])
+                        newDirpad2Walks.append(dirpad2Walks[w] + rob1Paths[1])
+                    dirpad2Walks = newDirpad2Walks
+                rob1 = rob1.find(c)
+            for w in dirpad2Walks:
+                newWalks.append(w)
+        minWalk = 99999999999999999999999999
+        for w in newWalks:
+            dirpadWalk = walk(dirPadS,w,dirGrid)
+            #print("DirpadWalk: {} len {}".format(dirpadWalk,len(dirpadWalk)))
+            keypadWalk = walk(dirPadS,dirpadWalk,dirGrid)
+            #print("KeypadWalk: {} len {}".format(keypadWalk,len(keypadWalk)))
+            foundKeypad = walk(keypadS,keypadWalk,keypad)
+            #print("Found Keypad: {} len {}".format(foundKeypad,len(foundKeypad)))
+            #print("{} {} len {}".format(foundKeypad,w,len(w)))
+            assert keypadNums == foundKeypad, f"Input Keypad was {keypadNums}"
+            #if len(w) < minWalk:
+            #    print(dirpadWalk)
+            #    print(keypadWalk)
+            #    print(foundKeypad)
+            minWalk = min(minWalk,len(w))
+        print("Res is {} * {}".format(minWalk,int(foundKeypad[0:3])))
+        total += minWalk*int(keypadNums[0:3])
+    return total
 
 def solve(filename, pt):
     print("-------------------------------------")
@@ -123,41 +205,40 @@ def solve(filename, pt):
             print("derivedKeypad: {} len {}".format(derivedKeypad,len(derivedKeypad)))
             print("Explicit input: {} Derived Input: {}".format(keypadNums,derivedKeypad))
         keypadWalk = ""
-        dirpad1Walk = ""
+        dirpad1Walk = []
         dirpad2Walk = ""
         rob3 = Point(keypadS.x,keypadS.y,keypadS.grid)
-        rob2 = Point(dirPadS.x,dirPadS.y,dirPadS.grid)
-        rob1 = Point(dirPadS.x,dirPadS.y,dirPadS.grid)
-        keypadWalks = []
+        rob2 = 'A'
         for c in keypadNums:
             rob3Path,rob3 = findPath(rob3,c)
             keypadWalk += rob3Path
-        for c in keypadWalk:
-            rob2Path = findDirpadPaths(rob2.value,c)[0]
-            rob2 = rob2.find(c)
-            dirpad1Walk += rob2Path
-        for c in dirpad1Walk:
-            rob1Path = findDirpadPaths(rob1.value,c)[0]
-            rob1 = rob1.find(c)
-            dirpad2Walk += rob1Path
-        print(dirpad2Walk)
-        dirpadWalk = walk(dirPadS,dirpad2Walk,dirGrid)
-        print("DirpadWalk: {} len {}".format(dirpadWalk,len(dirpadWalk)))
-        keypadWalk = walk(dirPadS,dirpadWalk,dirGrid)
-        print("KeypadWalk: {} len {}".format(keypadWalk,len(keypadWalk)))
-        foundKeypad = walk(keypadS,keypadWalk,keypad)
-        print("Found Keypad: {} len {}".format(foundKeypad,len(foundKeypad)))
-        assert keypadNums == foundKeypad, f"Input Keypad was {keypadNums}"
-        assert derivedKeypad == keypadNums, f"DerivedKeypad was {derivedKeypad}"
-        print("Res is {} * {}".format(len(dirpad2Walk),int(foundKeypad[0:3])))
-        total += len(dirpad2Walk)*int(keypadNums[0:3])
+        oldWalk = list(keypadWalk)
+        if pt == 1:
+            robots = 2
+        elif pt == 2:
+            robots = 25
+        for i in range(robots):
+            print("Robot: {} len Walk: {}".format(i,len(oldWalk)))
+            for c in oldWalk:
+                rob2Path = findDirpadPaths(rob2,c)
+                rob2 = c
+                for d in rob2Path:
+                    dirpad1Walk.append(d)
+            oldWalk = dirpad1Walk
+            dirpad1Walk = []
+        #for c in dirpad1Walk:
+        #    rob1Path = findDirpadPaths(rob1.value,c)[0]
+        #    rob1 = rob1.find(c)
+        #    dirpad2Walk += rob1Path
+        print("Res is {} * {}".format(len(oldWalk),int(keypadNums[0:3])))
+        total += len(oldWalk)*int(keypadNums[0:3])
     return total
 
 def run(pt,day,year,expect):
     with open("input.txt","w") as f:
         f.write(get_data(day=day,year=year))
-    resTest = solve("testInput.txt",pt)
-    assert resTest == expect, f"Result was {resTest}"
+    #resTest = solve("testInput.txt",pt)
+    #assert resTest == expect, f"Result was {resTest}"
     res = solve("input.txt",pt)
     print(res)
     #print("Test Input: {} Puzzle Input: {}".format(resTest,res))
@@ -168,4 +249,4 @@ def run(pt,day,year,expect):
     submit(res, part=c, day=day, year=year)
 
 run(1,21,2024,126384)
-#run(2,21,2024,-1)
+run(2,21,2024,-1)
